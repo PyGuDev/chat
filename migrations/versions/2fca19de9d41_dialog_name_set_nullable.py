9d925f8e4de329258f89b354959ea949dd1ade77
PyGuDev <gubaev.andreb@gmail.com>
2022-01-23 23:18:05 +0500

    Добавил миграции

---
+++ migrations/versions/2fca19de9d41_dialog_name_set_nullable.py
@@ -0,0 +1,24 @@
+"""Dialog name set nullable
+
+Revision ID: 2fca19de9d41
+Revises: 7cbd13b06bdb
+Create Date: 2022-01-23 21:34:55.594293
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2fca19de9d41'
+down_revision = '7cbd13b06bdb'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
