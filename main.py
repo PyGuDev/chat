9a8ca44adf20167c373d7ee76e084717ccf128ac
PyGuDev <gubaev.andreb@gmail.com>
2022-05-05 21:43:48 +0500

    добавил конфигурации для redis

--- main.py
+++ main.py
@@ -1,30 +1,42 @@
+from typing import Union
+
 from fastapi import FastAPI, HTTPException
-from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer
 from starlette import status
 from starlette.requests import Request
-from starlette.responses import Response
+from starlette.responses import Response, HTMLResponse
+from starlette.websockets import WebSocket
 
-from user.authenticate import get_current_user
 from core.db import SessionLocal
 from routes import routes
+from user.authenticate import get_user_by_token
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.middleware("http")
-async def db_session_middleware(request: Request, call_next):
+async def db_session_middleware(request: Union[Request, WebSocket], call_next):
     response = Response("Internal server error", status_code=500)
 
     try:
         request.state.db = SessionLocal()
-        
         if 'authorization' in list(request.headers.keys()):
             token = request.headers.get('Authorization')
             if token:
-                user = get_current_user(request.state.db, token)
+                user = get_user_by_token(request.state.db, token)
                 request.state.user = user
             else:
                 raise HTTPException(
--- settings.py
+++ settings.py
@@ -1,5 +1,17 @@
-SECRET_KEY = "00b7d9bee1f5b03461fbb65682773bff202e4cff1dcaca821fcb4dc6e787ce36"
-ACCESS_TOKEN_EXPIRE_MINUTES = 260
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ACCESS_TOKEN_EXPIRE_MINUTES = 8000
+REFRESH_TOKEN_EXPIRE_MINUTES = 8000
 
 USER_ID_FIELD = 'uid'
 USER_USERNAME_FIELD = 'phone'
+
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = os.getenv('REDIS_PORT')
+REDIS_PASS = os.getenv('REDIS_PASS')
